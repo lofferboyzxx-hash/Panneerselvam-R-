d3cab3e82f1047cc7f766b3b76071dbe268a73ff
lofferboyzxx-hash <lofferboyzxx@gmail.com>
2026-02-20 15:25:17 +0530

    Create Evenorodd.py

---
+++ Evenorodd.py
@@ -0,0 +1,10 @@
+# Program to Check Even or Odd
+
+# Take integer input from the user
+number = int(input("Enter an integer: "))
+
+# Check whether the number is even or odd
+if number % 2 == 0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
